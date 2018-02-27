0cb4cdf4d5500f5cc0e5c3e88257cffe0b37f476
Nícolas Matos <nicolasmatos0905@gmail.com>
2018-02-27 11:05:03 -0300

    Validando tokens para identificador

--- js/analisadorLexico.js
+++ js/analisadorLexico.js
@@ -5,27 +5,88 @@ function Token(tipo, valor, valido) {
     this.valido = valido;
 }
 
-//Instanciandoo um Token
-v1 = new Token(1, "Numerico", true);
-v2 = new Token(2, "Identificador", true);
-
 var tokensGlobais = [];
-
-tokensGlobais.push(v1);
-tokensGlobais.push(v2);
-
-console.log(tokensGlobais);
 
 //Função geral, responsavel pelo começo da validação do código
 function analisar() {
     var codigo = $("#codigo").val();
-    var quebra = codigo.split('');
+    var caracteres = codigo.split('');
 
-    var tokens = [];
+    while (caracteres.length > 0) {
+        //Convertendo o caracter lido para um inteiro
+        var caracter = caracteres[0].charCodeAt(0);
+        var caracterO = caracteres[0];
 
-    $.each(quebra, function( index, value ){
-        //alert(value.charCodeAt(0));
-        tokens.push(value.charCodeAt(0));
-    });
-    console.log(tokens);
+        //Objeto que receberá o token reconhecido
+        var token = new Token();
+
+        //Verificações para identificar o tipo do caracter lido
+        //Numerico
+        if (caracter >= 48 && caracter <= 57)
+        {
+            token = validarTokenNumerico(caracteres);
+        }
+
+        //Texto
+        else if (caracter == 34)
+        {
+            token = validarTokenTexto(caracteres);
+        }
+
+        //Identificador
+        else if ((caracter == 60) || (caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122))
+        {
+            token = validarTokenIdentificador(caracteres);
+        }
+
+        //Caracteres descartados
+        else if (caracter == 32)
+        {
+            token = new Token("ESPAÇO", caracterO, true);
+        }
+        else if (caracter == 10)
+        {
+            token = new Token("QUEBRA DE LINHA", caracterO, true);
+        }
+
+        for(i = 0; i < token.valor.length; i++)
+            caracteres.shift();
+
+        tokensGlobais.push(token);
+    }
+
+    console.log(tokensGlobais);
+}
+
+function validarTokenNumerico() {
+    
+}
+
+function validarTokenTexto() {
+
+}
+
+function validarTokenIdentificador(caracteres) {
+    var token = new Token("INDENTIFICADOR", "", "");
+    //PalavraReservada pr = new PalavraReservada();
+    var caracter = caracteres[0].charCodeAt(0);
+
+    if((caracter == 60) || (caracter == 62) || (caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)){
+        token.valor = caracteres[0];
+        for (i = 1; i < caracteres.length; i++)
+        {
+            caracter = caracteres[i].charCodeAt(0);
+            if ((caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122) || (caracter >= 48 && caracter <= 57) || caracter == 95 || caracter == 63)
+            {
+                token.valor = token.valor + caracteres[i];
+            }
+            else break;
+        }
+        token.valido = true;
+    }
+    else{
+        token.valido = false;
+    }
+
+    return token;
 }
