c6aecb29362197a685a7b7eb67d1963a9e1ed310
Naldo Maranhão <reginaldo.maranhao@iteva.org.br>
2018-02-27 07:10:50 -0300

    Quebrando String

--- js/processamento.js
+++ js/processamento.js
@@ -2,8 +2,37 @@ $(document).ready(function() {
     $("#aa").on("click", function () {
         var codigo = $("#codigo").val();
         var quebra = codigo.split('');
-
-        alert(quebra[5] == "\n");
+        processa(quebra);
     });
+
 });
 
+function processa(quebra) {
+    var tokens = [];
+    var i = 0;
+    var j = 0;
+    while (i != quebra.length){
+        if(quebra[i] != "\n") {
+            if (i == 0) {
+                tokens[j] = quebra[i];
+                i++;
+            }
+
+            else {
+                tokens[j] = tokens[j] + quebra[i];
+                i++;
+            }
+        }
+
+        else{
+            j++;
+            i++;
+        }
+    }
+
+    alert(tokens);
+
+    tokens.push("oi");
+    alert(tokens);
+
+}
