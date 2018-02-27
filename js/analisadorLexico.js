b9793d4c0c64a79ce2b27d6d8bc90ba103149d56
Nícolas Matos <nicolasmatos0905@gmail.com>
2018-02-27 11:40:17 -0300

    Validando tokens para string

--- js/analisadorLexico.js
+++ js/analisadorLexico.js
@@ -5,6 +5,8 @@ function Token(tipo, valor, valido) {
     this.valido = valido;
 }
 
+var palavrasReservadas = ['__halt_compiler', 'abstract', 'and', 'array', 'as', 'break', 'callable', 'case', 'catch', 'class', 'clone', 'const', 'continue', 'declare', 'default', 'die', 'do', 'echo', 'else', 'elseif', 'empty', 'enddeclare', 'endfor', 'endforeach', 'endif', 'endswitch', 'endwhile', 'eval', 'exit', 'extends', 'final', 'for', 'foreach', 'function', 'global', 'goto', 'if', 'implements', 'include', 'include_once', 'instanceof', 'insteadof', 'interface', 'isset', 'list', 'namespace', 'new', 'or', 'print', 'private', 'protected', 'public', 'require', 'require_once', 'return', 'static', 'switch', 'throw', 'trait', 'try', 'unset', 'use', 'var', 'while', 'xor'];
+
 var tokensGlobais = [];
 
 //Função geral, responsavel pelo começo da validação do código
@@ -22,35 +24,33 @@ function analisar() {
 
         //Verificações para identificar o tipo do caracter lido
         //Numerico
-        if (caracter >= 48 && caracter <= 57)
-        {
+        if (caracter >= 48 && caracter <= 57) {
             token = validarTokenNumerico(caracteres);
         }
 
-        //Texto
-        else if (caracter == 34)
-        {
-            token = validarTokenTexto(caracteres);
+        //String
+        else if (caracter == 34) {
+            token = validarTokenString(caracteres);
         }
 
         //Identificador
-        else if ((caracter == 60) || (caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122))
-        {
+        else if ((caracter == 60) || (caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)) {
             token = validarTokenIdentificador(caracteres);
         }
 
         //Caracteres descartados
-        else if (caracter == 32)
-        {
+        else if (caracter == 32) {
             token = new Token("ESPAÇO", caracterO, true);
         }
-        else if (caracter == 10)
-        {
+
+        else if (caracter == 10) {
             token = new Token("QUEBRA DE LINHA", caracterO, true);
         }
 
-        for(i = 0; i < token.valor.length; i++)
+        console.log(token);
+        for(i = 0; i < token.valor.length; i++) {
             caracteres.shift();
+        }
 
         tokensGlobais.push(token);
     }
@@ -58,17 +58,52 @@ function analisar() {
     console.log(tokensGlobais);
 }
 
-function validarTokenNumerico() {
-    
+function validarTokenNumerico(caracteres) {
+
 }
 
-function validarTokenTexto() {
+function validarTokenString(caracteres) {
+    var token = new Token("STRING", "", true);
+    var fechamento = false;
 
+    if (caracteres[0] == '\"')
+    {
+        for (i = 1; i < caracteres.length; i++)
+        {
+            if (token.valido)
+            {
+                if (caracteres[i] == '\n')
+                {
+                    token.valor += caracteres[i];
+                    token.valido = false;
+                }else if (caracteres[i] != '\"')
+                {
+                    token.valor += caracteres[i];
+                }
+                else
+                {
+                    token.valor = "\"" + token.valor + "\"";
+                    fechamento = true;
+                    break;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        if (!fechamento)
+        {
+            token.valido = false;
+        }
+    }
+    return token;
 }
 
 function validarTokenIdentificador(caracteres) {
-    var token = new Token("INDENTIFICADOR", "", "");
-    //PalavraReservada pr = new PalavraReservada();
+    var token = new Token("INDENTIFICADOR", "", true);
+    
     var caracter = caracteres[0].charCodeAt(0);
 
     if((caracter == 60) || (caracter == 62) || (caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)){
