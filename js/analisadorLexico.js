af61a2847c4132e6289cc0e0054f7fa4ed39946f
Nícolas Matos <nicolasmatos0905@gmail.com>
2018-02-27 16:27:17 -0300

    Validando tokens para comentários, e especiais

--- js/analisadorLexico.js
+++ js/analisadorLexico.js
@@ -1,7 +1,8 @@
 //Objeto para os Tokens
-function Token(tipo, valor, valido) {
+function Token(tipo, valor, valido, detalhe) {
     this.tipo = tipo;
     this.valor = valor;
+    this.detalhe = detalhe;
     this.valido = valido;
 }
 
@@ -40,11 +41,6 @@ function analisar() {
             token = validarTokenIdentificador(caracteres);
         }
 
-        //;
-        else if (caracter == 59) {
-            token = new Token("DELIMITADOR", caracterO, true);
-        }
-
         //Caracteres descartados
         else if (caracter == 32) {
             token = new Token("ESPAÇO", caracterO, true);
@@ -52,6 +48,24 @@ function analisar() {
 
         else if (caracter == 10) {
             token = new Token("QUEBRA DE LINHA", caracterO, true);
+        }
+
+        //Tokens especiais
+        else {
+            if (caracteres.length > 1) {
+                //Comentario
+                if (caracteres[0] + caracteres[1] == "//") {
+                    token = validarTokenComentario(caracteres);
+                }
+                else {
+                    //outros tokens
+                    token = validarTokenEspeciais(caracteres);
+                }
+            }
+            else {
+                //outros tokens
+                token = validarTokenEspeciais(caracteres);
+            }
         }
 
         //console.log(token);
@@ -66,42 +80,61 @@ function analisar() {
 }
 
 function validarTokenNumerico(caracteres) {
+    /*Token token = new Token("", Token.TipoDeToken.Numerico, "NUMERICO");
+    bool ponto = false;
 
+    for (int i = 0; i < subPrograma.Count(); i++)
+    {
+        int caracter = Convert.ToInt32(subPrograma[i]);
+        if (caracter >= 48 && caracter <= 57)
+        {
+            token.Valor += subPrograma[i];
+        }
+        else if (caracter == 46 && !ponto)
+        {
+            token.Valor += subPrograma[i];
+            ponto = true;
+        }else{
+            if (token.Valor.Length > 0)
+            {
+                break;
+            }
+            else
+            {
+                token.Valido = false;
+            }
+        }
+    }
+
+    return token;*/
 }
 
 function validarTokenString(caracteres) {
     var token = new Token("STRING", "", true);
     var fechamento = false;
 
-    if (caracteres[0] == '\"')
-    {
-        for (i = 1; i < caracteres.length; i++)
-        {
-            if (token.valido)
-            {
-                if (caracteres[i] == '\n')
-                {
+    if (caracteres[0] == '\"') {
+        for (i = 1; i < caracteres.length; i++) {
+            if (token.valido) {
+                if (caracteres[i] == '\n') {
                     token.valor += caracteres[i];
                     token.valido = false;
-                }else if (caracteres[i] != '\"')
-                {
+                }
+                else if (caracteres[i] != '\"') {
                     token.valor += caracteres[i];
                 }
-                else
-                {
+                else {
                     token.valor = "\"" + token.valor + "\"";
                     fechamento = true;
                     break;
                 }
             }
-            else
-            {
+            else {
                 break;
             }
         }
 
-        if (!fechamento)
-        {
+        if (!fechamento) {
             token.valido = false;
         }
     }
@@ -109,27 +142,114 @@ function validarTokenString(caracteres) {
 }
 
 function validarTokenIdentificador(caracteres) {
-    var token = new Token("INDENTIFICADOR", "", true);
+    var token = new Token("IDENTIFICADOR", "", true);
 
     var caracter = caracteres[0].charCodeAt(0);
 
     if (caracter == 63 && caracteres[1].charCodeAt(0) == 62) {
         token.valor = caracteres[0] + caracteres[1];
     }
-    else if((caracter == 60) || (caracter == 63) || (caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)){
+    else if ((caracter == 60) || (caracter == 63) || (caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122)) {
         token.valor = caracteres[0];
-        for (i = 1; i < caracteres.length; i++)
-        {
+        for (i = 1; i < caracteres.length; i++) {
             caracter = caracteres[i].charCodeAt(0);
-            if ((caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122) || (caracter >= 48 && caracter <= 57) || caracter == 95 || caracter == 63 || caracter == 62)
-            {
+            if ((caracter >= 65 && caracter <= 90) || (caracter >= 97 && caracter <= 122) || (caracter >= 48 && caracter <= 57) || caracter == 95 || caracter == 63 || caracter == 62) {
                 token.valor = token.valor + caracteres[i];
             }
             else break;
         }
         token.valido = true;
     }
-    else{
+    else {
+        token.valido = false;
+    }
+
+    return token;
+}
+
+function validarTokenComentario(caracteres) {
+    var token = new Token("COMENTÁRIO", "", true);
+
+    //Verifica se o subprograma é maior que zero
+    if (caracteres.length > 1) {
+        //Verifica se os dois primeiros caracteres são //
+        if (caracteres[0] == '/' && caracteres[1] == '/') {
+            token.valor = "//";
+
+            //Concatena todos os caracteres no comentario até achar um \n
+            for (i = 2; i < caracteres.length; i++) {
+                if (caracteres[i] != '\n') {
+                    token.valor += caracteres[i];
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        else {
+            token.valido = false;
+        }
+    }
+    else {
+        token.valido = false;
+    }
+
+
+    return token;
+}
+
+function validarTokenEspeciais(caracteres) {
+    var operadoresUmChar = [], operadoresDoisChar = [];
+
+    operadoresUmChar.push(
+        new Token("OPERADOR", "+", true, "OPERADOR DE ADIÇÃO"),
+        new Token("OPERADOR", "-", true, "OPERADOR DE SUBTRAÇÃO"),
+        new Token("OPERADOR", "*", true, "OPERADOR DE MULTIPLICAÇÃO"),
+        new Token("OPERADOR", "/", true, "OPERADOR DE DIVISÃO"),
+        new Token("OPERADOR", "%", true, "OPERADOR DE RESTO"),
+        new Token("OPERADOR", "^", true, "OPERADOR DE POTENCIAÇÃO"),
+        new Token("OPERADOR", "<", true, "OPERADOR MENOR QUE"),
+        new Token("OPERADOR", ">", true, "OPERADOR MAIOR QUE"),
+        new Token("ATRIBUIÇÃO", "=", true, "OPERADOR DE ATRIBUIÇÃO"),
+        new Token("PONTUAÇÃO", ",", true, "SEPARADOR DE ARGUMENTOS"),
+        new Token("PONTUAÇÃO", ";", true, "FINALIZADOR DE COMANDO"),
+        new Token("PARENTIZADOR","{", true, "ABERTURA DE CHAVE"),
+        new Token("PARENTIZADOR","[", true, "ABERTURA DE COLCHETE"),
+        new Token("PARENTIZADOR","(", true, "ABERTURA DE PARENTESE"),
+        new Token("PARENTIZADOR",")", true, "FECHAMENTO DE PARENTESE"),
+        new Token("PARENTIZADOR","]", true, "FECHAMENTO DE COLCHETE"),
+        new Token("PARENTIZADOR","}", true, "FECHAMENTO DE CHAVE")
+    );
+
+    operadoresDoisChar.push(
+        new Token("OPERADOR", "++", true, "OPERADOR DE INCREMENTO"),
+        new Token("OPERADOR", "--", true, "OPERADOR DE DECREMENTO"),
+        new Token("OPERADOR", ">=", true, "COMPARADOR MAIOR OU IGUAL QUE"),
+        new Token("OPERADOR", "<=", true, "COMPARADOR MENOR OU IGUAL QUE"),
+        new Token("OPERADOR", "==", true, "COMPARADOR IGUALDADE"),
+        new Token("OPERADOR", "&&", true, "OPERADOR E"),
+        new Token("OPERADOR", "||", true, "OPERADOR OU"),
+        new Token("OPERADOR", "!=", true, "COMPARADOR DIFERENTE")
+    );
+
+    var token = new Token("DESCONHECIDO", " ", true, "");
+
+    $.each(operadoresUmChar, function( key, value ) {
+        if(value.valor == caracteres[0]) {
+            token = value;
+        }
+    });
+
+    if (caracteres.length > 1) {
+        $.each(operadoresDoisChar, function( key, value ) {
+            if(value.valor == (caracteres[0] + caracteres[1])) {
+                token = value;
+            }
+        });
+    }
+
+    if (token.valor == " ") {
+        token.valor = caracteres[0];
         token.valido = false;
     }
 
